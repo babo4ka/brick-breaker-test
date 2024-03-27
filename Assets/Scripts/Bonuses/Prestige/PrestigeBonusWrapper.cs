75e07d5818f1bee287d49e824ef79446497b478b
basra4ka <evgeha_ivanov_01@bk.ru>
2024-03-27 16:28:51 +0300

    pbw

---
+++ Assets/Scripts/Bonuses/Prestige/PrestigeBonusWrapper.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrestigeBonusWrapper : MonoBehaviour
+{
+    private PrestigeBonusType type;
+
+    private PrestigeBonus prestigeBonus;
+
+    private PrestigeBonusWrapper left;
+    private PrestigeBonusWrapper right;
+
+
+}
